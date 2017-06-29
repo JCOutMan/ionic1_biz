88924c1d71cc54cb378ff1537136b0664a24ef3b
JCOutMan <504804228@qq.com>
2017-06-30 00:58:29 +0800

    'commit'

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,22 @@
 var gulp = require('gulp');
-var gutil = require('gulp-util');
-var bower = require('bower');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var sh = require('shelljs');
+var browserSync = require('browser-sync').create();
+var proxy = require('http-proxy-middleware');
+var express = require('express');
+var app = express();
 
-var paths = {
-  sass: ['./scss/**/*.scss']
-};
-
-gulp.task('default', ['sass']);
-
-gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .on('error', sass.logError)
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
-});
-
-gulp.task('watch', ['sass'], function() {
-  gulp.watch(paths.sass, ['sass']);
-});
-
-gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
-    .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+gulp.task('browser-sync', () => {
+// 静态服务器
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
     });
 });
 
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
-  }
-  done();
-});
+gulp.task('http-proxy', () => {
+  app.use('/api', proxy({target:'http://www.example.org/api', changeOrigin:true}));
+  app.get('/', '')
+  app.listen(3000);
+})
+
+gulp.task('default', ['browser-sync', 'http-proxy']);
