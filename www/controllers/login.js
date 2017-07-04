ba05e5c79cc9d20a1ae57da5a6a9ee73fc297900
JCOutMan <504804228@qq.com>
2017-07-04 23:57:03 +0800

    add file

---
+++ www/controllers/login.js
@@ -0,0 +1,6 @@
+/**
+ * Created by JC on 2017/6/29.
+ */
+angular.module('starter.controllers').controller('login', function($scope){
+
+});
---
+++ www/pages/login/loginServ.js
@@ -0,0 +1,9 @@
+/**
+ * Created by dagecheng on 2017/6/30.
+ */
+angular.module('starter.services', [])
+  .service('loginServ', function(){
+      return {
+
+      }
+  });
